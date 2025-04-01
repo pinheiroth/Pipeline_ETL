8a0b7f6d1d64e83d8d60d1d7941b7d693f5dc19a
Admin <pinheiropth@gmail.com>
2025-04-01 16:06:15 -0300

    adc SQlite3

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
+import sqlite3
 
 def fetch_page():
     url = "https://www.mercadolivre.com.br/macbook-pro-macbook-pro-14-m3-pro-14-space-black-18gb-de-ram-512gb-ssd-apple-m3/p/MLB28766778#polycard_client=search-nordic&searchVariation=MLB28766778&wid=MLB5065017432&position=4&search_layout=grid&type=product&tracking_id=f4539774-3f30-4a51-93c9-c8c5caa7c302&sid=search"
@@ -26,19 +27,39 @@ def parse_page(html):
         'timestamp': timestamp
     }
 
-def df_save(produto_value, df):
+def create_conection(db_name = "macbook_price.db"):
+    """Cria uma conexão com o banco de dados SQlite. """
+    conn = sqlite3.connect(db_name)
+    return conn
+
+def setup_db(conn):
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS prices (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product_name TEXT,
+            old_price REAL,
+            installment_price REAL,
+            timestamp TEXT
+        )
+    ''')
+    conn.commit()
+    
+
+def db_save(conn, produto_value):
     new_row = pd.DataFrame([produto_value])
-    df = pd.concat([df, new_row], ignore_index = True)
-    return df
+    new_row.to_sql('prices', conn, if_exists='append', index=False)
 
 if __name__ == "__main__":
 
-    df = pd.DataFrame()
+    conn = create_conection()
+    setup_db(conn)
 
     while True:
         page_content = fetch_page()
         produto_value = parse_page(page_content)
-        df = df_save(produto_value, df)
+        db_save(conn, produto_value)
+        print(f"Dados salvos com sucesso: {produto_value}")
         time.sleep(10)
 
-        print(df)
+        
